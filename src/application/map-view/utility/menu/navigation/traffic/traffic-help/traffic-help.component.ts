edd03136d19b99abdc89aadf9c0de5de04a3db75
Afagh Heidarian <afag.heidarian@yahoo.com>
2020-07-12 14:21:30 +0430

    traffic

--- src/application/map-view/utility/menu/navigation/traffic/traffic-help/traffic-help.component.ts
+++ src/application/map-view/utility/menu/navigation/traffic/traffic-help/traffic-help.component.ts
@@ -1,32 +1,49 @@
+import { animate, transition, trigger, style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { MapService } from 'src/application/shared/services/map.service';
 import { PublicVarService } from 'src/application/shared/services/public-var.service';
 import { TrafficComponent } from './../traffic.component';
-import { MapService } from 'src/application/shared/services/map.service';
+
 
 @Component({
- selector: 'app-traffic-help',
- templateUrl: './traffic-help.component.html',
- styleUrls: [
-  './traffic-help.component.scss',
- ],
+   selector: 'app-traffic-help',
+   templateUrl: './traffic-help.component.html',
+   styleUrls: [
+      './traffic-help.component.scss',
+   ],
+   animations: [
+      trigger('openCloseTraffic', [
+         transition(':enter', [
+           style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]
+         ),
+         transition(':leave',
+           [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]
+         )
+       ])
+   ],
 })
 export class TrafficHelpComponent implements OnInit {
- constructor(public publicVar: PublicVarService, public traffic: TrafficComponent, private mapservice: MapService) {}
+   constructor(public publicVar: PublicVarService, public traffic: TrafficComponent, private mapservice: MapService) { }
 
- ngOnInit() {
-  this.updateTrafficdata();
- }
- // baraye anke dadeh traffic baraye har change zoom update shavad
- // bayad hatman tag component dakhel base map bashad bakhate hamin in function inja neveshtim
- updateTrafficdata() {
-  this.mapservice.map.on('moveend', (evt: Event) => {
-   console.log('update');
-   if (this.publicVar.isTrafficON) {
-    const source = this.publicVar.WMTSLayerTraffic.getSource();
-    const params = source.getParams();
-    params.t = Math.random();
-    source.updateParams(params);
+   ngOnInit() {
+      this.updateTrafficdata();
    }
-  });
- }
+   // baraye anke dadeh traffic baraye har change zoom update shavad
+   // bayad hatman tag component dakhel base map bashad bakhate hamin in function inja neveshtim
+   updateTrafficdata() {
+      //   this.mapservice.map.on('moveend', (evt: Event) => {
+      //   });
+      setInterval(() => {
+         console.log('update');
+         if (this.publicVar.isTrafficON) {
+            const source = this.publicVar.WMTSLayerTraffic.getSource();
+            const params = source.getParams();
+            params.t = Math.random();
+            source.updateParams(params);
+         }
+      },20000)
+   }
 }
+
+
+
--- src/application/map-view/utility/menu/navigation/traffic/traffic.component.ts
+++ src/application/map-view/utility/menu/navigation/traffic/traffic.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PublicVarService } from 'src/application/shared/services/public-var.service';
 import { MapService } from 'src/application/shared/services/map.service';
-
+import { PublicVarService } from 'src/application/shared/services/public-var.service';
 @Component({
  selector: 'app-traffic',
  template: `<label class="switch">
@@ -22,7 +21,6 @@ export class TrafficComponent implements OnInit {
   if (TrafficInput.checked) {
    this.publicVar.isTrafficON = true;
    this.publicVar.isTrafficHelpON = true;
-
    this.mapservice.map.addLayer(this.publicVar.WMTSLayerTraffic);
    console.log('Traffic.checked');
   } else {
@@ -30,7 +28,7 @@ export class TrafficComponent implements OnInit {
    this.mapservice.map.removeLayer(this.publicVar.WMTSLayerTraffic);
    setTimeout(() => {
     this.publicVar.isTrafficHelpON = false;
-   }, 1500);
+   }, 300);
   }
   this.publicVar.status.traffic = this.publicVar.isTrafficON;
   localStorage.setItem('Status', JSON.stringify(this.publicVar.status));
